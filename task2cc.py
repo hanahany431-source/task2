f4cdcd353f18653144033bb1443cbfd33e944988
hanahany431-source <hanahany431@gmail.com>
2026-06-10 19:25:04 +0300

    Delete task2cc.py

--- task2cc.py
+++
@@ -1,9 +0,0 @@
-temperature = int(input("Enter the temperature in Celsius: "))
-if temperature >= 30:
-    print("It's a hot day. Stay hydrated! ")
-elif 20 <= temperature <= 29:
-    print("It's a warm day. Enjoy the weather! ")
-elif 10 <= temperature <= 19:
-    print("It's a cool day. Wear a jacket! ")
-else
-    print("It's a cold day. Stay warm! ")
